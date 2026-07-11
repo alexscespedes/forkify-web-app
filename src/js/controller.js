f3ef07a19c59466368443182a75abca40fe452df
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2026-07-11 09:41:43 -0400

    Add Error & Success Messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,7 +22,7 @@ const controlRecipies = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
