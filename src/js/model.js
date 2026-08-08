55b450b49a7506ce6c860d41fa5903b3fb57b992
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2026-08-08 09:49:57 -0400

    Fix bug of page search results to reset to 1 after reloading the page

--- src/js/model.js
+++ src/js/model.js
@@ -48,6 +48,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+    state.search.page = 1;
     console.log(state.search.results);
   } catch (err) {
     console.error(`${err} 💥💥💥💥`);
